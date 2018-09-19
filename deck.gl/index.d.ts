88f073622bb6761880618c33fa5f2eca1471ffe1
Dan Marshall <danmar@microsoft.com>
2018-09-19 16:08:15 -0700

    add OrbitController to experimental

--- deck.gl/index.d.ts
+++ deck.gl/index.d.ts
@@ -3,10 +3,10 @@
 /// <reference path="../@deck.gl/react/index.d.ts" />
 
 declare module 'deck.gl' {
-	 const experimental: {};
+	import OrbitController from '@deck.gl/core/controllers/orbit-controller';
+	const experimental: { OrbitController };
 	export { COORDINATE_SYSTEM, Deck, Layer, CompositeLayer, View, MapView, FirstPersonView, ThirdPersonView, OrbitView, OrbitViewState, PerspectiveView, OrthographicView, Viewport, WebMercatorViewport, PerspectiveViewport, OrthographicViewport, Controller, MapController, AttributeManager, project, project64, lighting, LayerManager, log } from '@deck.gl/core';
 	export { ArcLayer, IconLayer, LineLayer, PointCloudLayer, ScatterplotLayer, ScreenGridLayer, GridLayer, GridCellLayer, HexagonLayer, HexagonCellLayer, PathLayer, PolygonLayer, GeoJsonLayer, TextLayer } from '@deck.gl/layers';
 	export { default, DeckGL } from '@deck.gl/react';
 	export { experimental };
-
 }
