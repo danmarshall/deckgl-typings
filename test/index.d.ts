21791d4ac2603b658b63e9f6306e9470f8a0538d
Dan Marshall <danmar@microsoft.com>
2018-10-25 00:52:09 -0700

    added test

---
+++ test/index.d.ts
@@ -0,0 +1,18 @@
+/// <reference path="../luma.gl/index.d.ts" />
+/// <reference path="../deck.gl__core/index.d.ts" />
+
+import {
+    COORDINATE_SYSTEM,
+    Deck,
+    Layer,
+    LineLayer,
+    OrbitView,
+    PolygonLayer,
+    TextLayer
+} from 'deck.gl';
+import {
+    CubeGeometry,
+    fp64,
+    GL,
+    Model
+} from 'luma.gl';
