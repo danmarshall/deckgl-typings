598dd2c841a5a29760acf13da4229712061a462a
Dan Marshall <danmar@microsoft.com>
2018-12-13 22:08:03 -0800

    copy exports from source

--- deck.gl/index.d.ts
+++ deck.gl/index.d.ts
@@ -2,8 +2,69 @@
 /// <reference path="../deck.gl__layers/index.d.ts" />
 /// <reference path="../deck.gl__react/index.d.ts" />
 
+import { } from '@deck.gl/core';
+
 declare module 'deck.gl' {
-	export { COORDINATE_SYSTEM, Deck, Layer, CompositeLayer, View, MapView, FirstPersonView, ThirdPersonView, OrbitView, PerspectiveView, OrthographicView, Viewport, WebMercatorViewport, Controller, MapController, AttributeManager, project, project64, lighting, LayerManager, log, _OrbitController, _FirstPersonController, TRANSITION_EVENTS, LinearInterpolator, FlyToInterpolator, _EffectManager, _Effect, _ReflectionEffect } from '@deck.gl/core';
-	export { ArcLayer, IconLayer, LineLayer, PointCloudLayer, ScatterplotLayer, ScreenGridLayer, GridLayer, GridCellLayer, HexagonLayer, HexagonCellLayer, PathLayer, PolygonLayer, GeoJsonLayer, TextLayer } from '@deck.gl/layers';
+	export { experimental } from '@deck.gl/core';
+	export {
+		// CONSTANTS
+		COORDINATE_SYSTEM,
+		// Main class
+		Deck,
+		// Base Layers
+		Layer,
+		CompositeLayer,
+		// Views
+		View,
+		MapView,
+		FirstPersonView,
+		ThirdPersonView,
+		OrbitView,
+		PerspectiveView,
+		OrthographicView,
+		// Viewports
+		Viewport,
+		WebMercatorViewport,
+		// Controllers
+		Controller,
+		MapController,
+		// For custom layers
+		AttributeManager,
+		// Shader modules
+		project,
+		project64,
+		lighting,
+		// Internal classes
+		LayerManager,
+		// Logging
+		log,
+		// Controllers
+		_OrbitController,
+		_FirstPersonController,
+		// Transition bindings
+		TRANSITION_EVENTS,
+		LinearInterpolator,
+		FlyToInterpolator,
+		// Effects
+		_EffectManager,
+		_Effect,
+		_ReflectionEffect
+	} from '@deck.gl/core';
+	export {
+		ArcLayer,
+		IconLayer,
+		LineLayer,
+		PointCloudLayer,
+		ScatterplotLayer,
+		ScreenGridLayer,
+		GridLayer,
+		GridCellLayer,
+		HexagonLayer,
+		HexagonCellLayer,
+		PathLayer,
+		PolygonLayer,
+		GeoJsonLayer,
+		TextLayer
+	} from '@deck.gl/layers';
 	export { default, DeckGL } from '@deck.gl/react';
 }
