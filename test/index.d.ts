bcb13d7611263c9ea17414b997a8034ce3e308bb
Dan Marshall <danmar@microsoft.com>
2019-11-04 16:04:43 -0800

    individual tests

--- test/index.d.ts
+++ test/index.d.ts
@@ -1,17 +1,1 @@
-/// <reference path="../luma.gl/index.d.ts" />
-/// <reference path="../deck.gl__core/index.d.ts" />
-
-import {
-    COORDINATE_SYSTEM,
-    Deck,
-    Layer,
-    LineLayer,
-    OrbitView,
-    PolygonLayer,
-    TextLayer
-} from 'deck.gl';
-import {
-    CubeGeometry,
-    fp64,
-    Model
-} from 'luma.gl';
+/// <reference path="./math.gl.d.ts" />
---
+++ test/math.gl.d.ts
@@ -0,0 +1,5 @@
+/// <reference path="../math.gl/index.d.ts" />
+
+import * as MathGl from 'math.gl';
+
+type m = MathGl.Vector3;
