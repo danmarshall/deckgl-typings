a87dd0a0d975ec07c0902c2d691dff4891d2fcd1
Dan Marshall <danmar@microsoft.com>
2019-11-04 16:40:46 -0800

    rename with @ symbol

--- test/index.d.ts
+++ test/index.d.ts
@@ -1,4 +1,4 @@
 /// <reference path="./math.gl.d.ts" />
-/// <reference path="./luma.gl/webgl.d.ts" />
-/// <reference path="./luma.gl/webgl-state-tracker.d.ts" />
-/// <reference path="./luma.gl/core.d.ts" />
+/// <reference path="./@luma.gl/webgl.d.ts" />
+/// <reference path="./@luma.gl/webgl-state-tracker.d.ts" />
+/// <reference path="./@luma.gl/core.d.ts" />
