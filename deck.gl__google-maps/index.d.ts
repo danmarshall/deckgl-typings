7d55d25f7576869a28f6eebdcad99fe951270035
Dan Marshall <danmar@microsoft.com>
2020-04-29 21:45:37 -0700

    v8

--- deck.gl__google-maps/index.d.ts
+++ deck.gl__google-maps/index.d.ts
@@ -1,4 +1,4 @@
-//typings for @deck.gl/google-maps v7.3.3
+//typings for @deck.gl/google-maps v8.1.3
 declare module '@deck.gl/google-maps/utils' {
 	/**
 	 * Get a new deck instance
@@ -18,29 +18,29 @@ declare module '@deck.gl/google-maps/utils' {
 	 * @param overlay (google.maps.OverlayView) - A maps Overlay instance
 	 */
 	export function getViewState(map: any, overlay: any): {
-		width: any;
-		height: any;
-		left: any;
-		top: any;
-		zoom: number;
-		pitch: any;
-		latitude: any;
-		longitude: any;
+	    width: any;
+	    height: any;
+	    left: any;
+	    top: any;
+	    zoom: number;
+	    pitch: any;
+	    latitude: any;
+	    longitude: any;
 	};
 
 }
 declare module '@deck.gl/google-maps/google-maps-overlay' {
 	export default class GoogleMapsOverlay {
-		constructor(props: any);
-		setMap(map: any): void;
-		setProps(props: any): void;
-		pickObject(params: any): any;
-		pickMultipleObjects(params: any): any;
-		pickObjects(params: any): any;
-		finalize(): void;
-		_onAdd(): void;
-		_onRemove(): void;
-		_draw(): void;
+	    constructor(props: any);
+	    setMap(map: any): void;
+	    setProps(props: any): void;
+	    pickObject(params: any): any;
+	    pickMultipleObjects(params: any): any;
+	    pickObjects(params: any): any;
+	    finalize(): void;
+	    _onAdd(): void;
+	    _onRemove(): void;
+	    _draw(): void;
 	}
 
 }
