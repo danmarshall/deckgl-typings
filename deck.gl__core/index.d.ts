a75d5faf54b4e7afbbd1304ee348fa6b52f74907
Steven Roussey <42726156+stevenroussey-privicy@users.noreply.github.com>
2020-07-06 18:40:19 -0700

    ViewStateProps (#101)
    * ViewStateProps
    
    * ViewStateProps with transitions
    
    * everything optional
    
    * Add some less documented options

--- deck.gl__core/index.d.ts
+++ deck.gl__core/index.d.ts
@@ -2198,6 +2198,8 @@ declare module "@deck.gl/core/lib/deck" {
 	import Layer from "@deck.gl/core/lib/layer";
 	import View from "@deck.gl/core/views/view";
 	import Viewport from "@deck.gl/core/viewports/viewport";
+	import TransitionInterpolator from "@deck.gl/core/transitions/transition-interpolator";
+	import { TRANSITION_EVENTS } from "@deck.gl/core/controllers/transition-manager";
 
 	export interface InteractiveState {
 		isDragging: boolean;
@@ -2210,6 +2212,31 @@ declare module "@deck.gl/core/lib/deck" {
 		x: number;
 		y: number;
 		coordinate?: {};
+	}
+
+	// https://deck.gl/docs/api-reference/core/deck#viewstate
+	export interface InitialViewStateProps {
+		latitude?: number;
+		longitude?: number;
+		zoom?: number;
+		bearing?: number;
+		pitch?: number;
+		altitude?: number;
+		maxPitch?: number;
+		maxZoom?: number;
+		minPitch?: number;
+		minZoom?: number;	  
+	}
+
+	// https://github.com/visgl/deck.gl/blob/master/docs/developer-guide/view-state-transitions.md
+	export interface ViewStateProps extends InitialViewStateProps {
+		transitionDuration?: number | string;
+		transitionEasing?: (x: number)=> number;
+		transitionInterpolator?: TransitionInterpolator;
+		transitionInterruption?: typeof TRANSITION_EVENTS;
+		onTransitionStart?: () => void;
+		onTransitionInterrupt?: () => void;
+		onTransitionEnd?: () => void;
 	}
 
 	export interface DeckProps {
@@ -2226,8 +2253,8 @@ declare module "@deck.gl/core/lib/deck" {
 		}) => boolean;
 		getCursor?: (interactiveState: InteractiveState) => string;
 		views?: View[];
-		viewState?: any;
-		initialViewState?: any;
+		viewState?: ViewStateProps;
+		initialViewState?: InitialViewStateProps;
 		controller?: null | Controller | ControllerOptions | boolean;
 		effects: Effect[];
 
