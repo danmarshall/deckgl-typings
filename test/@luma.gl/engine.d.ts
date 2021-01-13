6511d7f50bbf65cd990173d74a6dc8c191a4c62e
Dan Marshall <danmar@microsoft.com>
2021-01-13 12:24:46 -0800

    remove conflicts with official luma.gl typings (#147)

--- test/@luma.gl/engine.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference path="../../luma.gl__engine/index.d.ts" />
-
-import { AnimationLoop, ClipSpace } from '@luma.gl/engine';
--- test/@luma.gl/experimental.d.ts
+++
@@ -1,3 +0,0 @@
-/// <reference path="../../luma.gl__experimental/index.d.ts" />
-
-import { Display, GLTFEnvironment } from '@luma.gl/experimental';
