9910a7806df8bf788dfdadc54de07e2d6267a027
j-esse <j-esse@users.noreply.github.com>
2021-02-23 02:21:36 -0600

    View Props (#159)
    * View Props
    
    See https://deck.gl/docs/api-reference/core/view#constructor-1  and https://github.com/visgl/deck.gl/blob/c60064d4e60bda83ee3dc276e421d4e1faa08896/modules/core/src/views/view.js#L7
    
    * Update index.d.ts
    
    * Update index.d.ts

--- deck.gl__core/index.d.ts
+++ deck.gl__core/index.d.ts
@@ -1542,8 +1542,34 @@ declare module "@deck.gl/core/utils/positions" {
 	export function getPosition(position: any, extent: any): any;
 }
 declare module "@deck.gl/core/views/view" {
+
+	export interface ViewProps {
+		id?: string;
+
+		// The position and extent of the view on the canvas
+		x?: number | string; // A relative (e.g. `'50%'`) or absolute position. Default `0`.
+		y?: number | string; // A relative (e.g. `'50%'`) or absolute position. Default `0`.
+		height?: number | string; // A relative (e.g. `'50%'`) or absolute extent. Default `'100%'`.
+		width?: number | string; // A relative (e.g. `'50%'`) or absolute extent. Default `'100%'`.
+
+		controller?: boolean | object; // Options for viewport interactivity.
+
+		// Viewport Options
+		projectionMatrix?: number[]; // 4x4 Projection matrix - Array[16]
+
+		// Perspective projection parameters, used if projectionMatrix not supplied
+		fovy?: number; // Field of view covered by camera, in the perspective case. In degrees. Default `50`.
+		aspect?: number; // Aspect ratio. Defaults to the Viewport's `width/height` ratio.
+		near?: number; // Distance of near clipping plane. Default `0.1`. (Note that in geospatial viewports, this actual distance used is scaled by the height of the screen).
+		far?: number; // Distance of far clipping plane. Default `1000`. (Note that in geospatial viewports, this actual distance used is scaled by the height of the screen).
+
+		focalDistance?: number; // Modifier of viewport scale. Corresponds to the number of pixels per meter. Default `1`.
+
+		modelMatrix?: number[]; // A model matrix to be applied to position, to match the layer props API
+	}
+
 	export default class View {
-		constructor(props?: {});
+		constructor(props?: ViewProps);
 		equals(view: any): any;
 		makeViewport({
 			width,
@@ -2723,8 +2749,11 @@ declare module "@deck.gl/core/controllers/orbit-controller" {
 	}
 }
 declare module "@deck.gl/core/views/orbit-view" {
-	import View from "@deck.gl/core/views/view";
+	import View, { ViewProps } from "@deck.gl/core/views/view";
 	import { Position } from "@deck.gl/core/utils/positions";
+	export interface OrbitViewProps extends ViewProps {
+		orbitAxis?: "Z" | "Y"; // Axis with 360 degrees rotating freedom, either `'Y'` or `'Z'`, default to `'Z'`.
+	}
 	export interface OrbitViewState {
 		target: Position;
 		rotationOrbit?: number;
@@ -2736,7 +2765,7 @@ declare module "@deck.gl/core/views/orbit-view" {
 		maxRotationX?: number;
 	}
 	export default class OrbitView extends View {
-		constructor(props: any);
+		constructor(props: OrbitViewProps);
 		get controller(): any;
 	}
 }
