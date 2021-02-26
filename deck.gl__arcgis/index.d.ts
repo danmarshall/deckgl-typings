8c3fe488060436f939704011cf46857410031a19
Dan Marshall <danmar@microsoft.com>
2021-02-26 01:08:13 -0800

    add arcgis & carto (#171)
    * add arcgis & carto
    
    * add @deck.gl/arcgis

---
+++ deck.gl__arcgis/index.d.ts
@@ -0,0 +1,40 @@
+//typings for @deck.gl/arcgis v8.4.9
+declare module '@deck.gl/arcgis/commons' {
+	export function initializeResources(gl: any): void;
+	export function render({ gl, width, height, viewState }: {
+	    gl: any;
+	    width: any;
+	    height: any;
+	    viewState: any;
+	}): void;
+	export function finalizeResources(): void;
+
+}
+declare module '@deck.gl/arcgis/deck-layer-view-2d' {
+	export default function createDeckLayerView2D(BaseLayerViewGL2D: any): any;
+
+}
+declare module '@deck.gl/arcgis/deck-layer' {
+	export default function createDeckLayer(DeckProps: any, Layer: any, DeckLayerView2D: any): any;
+
+}
+declare module '@deck.gl/arcgis/deck-props' {
+	export default function loadDeckProps(Accessor: any): any;
+
+}
+declare module '@deck.gl/arcgis/deck-renderer' {
+	export interface IDeckRenderer {
+	    view: any;
+	    deck: any;
+	    setup(context: any): any;
+	    dispose(): any;
+	    redraw(): any;
+	    render(context: any): any;
+	}
+	export default function createDeckRenderer(DeckProps: any, externalRenderers: any): IDeckRenderer;
+
+}
+declare module '@deck.gl/arcgis' {
+	export function loadArcGISModules(modules: any, loadScriptOptions: any): Promise<any>;
+
+}
---
+++ test/@deck.gl/carto.d.ts
@@ -0,0 +1,3 @@
+/// <reference path="../../deck.gl__carto/index.d.ts" />
+
+import { CartoSQLLayer, getDefaultCredentials, setDefaultCredentials } from '@deck.gl/carto';
