d4063b58f13a3f071afb10211074c058ea40c9d7
Shailesh Gavathe <shailesh.gavathe@asetpartners.com>
2022-04-13 21:45:50 -0400

    updates to also aloow the ESRI layer from arcgiscore directly

--- deck.gl__arcgis/index.d.ts
+++ deck.gl__arcgis/index.d.ts
@@ -25,5 +25,25 @@ declare module '@deck.gl/arcgis/deck-renderer' {
   export default function createDeckRenderer(DeckProps: any, externalRenderers: any): IDeckRenderer;
 }
 declare module '@deck.gl/arcgis' {
-  export function loadArcGISModules(modules: any, loadScriptOptions: any): Promise<any>;
-}
+    export function loadArcGISModules(modules: any, loadScriptOptions: any): Promise<any>;
+    export class DeckLayer {
+      constructor(gl: any);
+      setProps(props: any): void;
+      renderLayers(opts: any): void;
+      needsRedraw(opts?: { clearRedrawFlags: boolean }): any;
+      finalize(): void;
+      _preRender(effects: any, opts: any): void;
+      _resizeRenderBuffers(): void;
+      _postRender(effects: any, opts: any): void;
+    };
+    export class DeckRenderer {
+      constructor(gl: any);
+      setProps(props: any): void;
+      renderLayers(opts: any): void;
+      needsRedraw(opts?: { clearRedrawFlags: boolean }): any;
+      finalize(): void;
+      _preRender(effects: any, opts: any): void;
+      _resizeRenderBuffers(): void;
+      _postRender(effects: any, opts: any): void;
+    };
+  }
